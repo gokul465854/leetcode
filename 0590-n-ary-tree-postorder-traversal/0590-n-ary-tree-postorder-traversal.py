89749fc3397d9735e3510732201cab987c012727
Gokul Gaikwad <gokulgaikwad416@gmail.com>
2026-08-28 16:31:18 +0530

    Time: 49 ms (86.01%), Space: 20.9 MB (59.36%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        
+        ans = []
+        stack = [root]
+        
+        while stack:
+
+            node = stack.pop()
+            ans.append(node.val)
+
+            for child in node.children:
+                stack.append(child)
+
+        ans.reverse()
+        return  ans
